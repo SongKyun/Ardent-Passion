ca3c80fdcdae882bc6f1af5af92893ff21720a58
araken@naver.com <araken@naver.com>
2022-11-09 23:40:19 +0900

    2022-11-9
    복사 생성자

---
+++ Ardent Passion Chapter 05/Ardent Passion Chapter 05/05-1.cpp
@@ -0,0 +1,19 @@
+//NameCard(char* name, char* company, char* phone, int pos)
+//	: position(pos)
+//{
+//	this->name = new char[strlen(name) + 1];
+//	this->company = new char[strlen(company) + 1];
+//	this->phone = new char[strlen(phone) + 1];
+//	strcpy(this->name, name);
+//	strcpy(this->company, _company);
+//	strcpy(this->phone, _phone);
+//}
+//NameCard(const NameCard& ref) : position(ref.position)
+//{
+//	name = new char[strlen)(ref.name) + 1];
+//	company = new ...
+//	phone = new ...
+//	strcpy(name, ref.name);
+//	strcpy ..
+//	strcpy ..
+//}

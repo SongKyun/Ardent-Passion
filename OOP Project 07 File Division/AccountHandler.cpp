4498fe303981a2f0858bd04345b04fed643fa3d5
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2022-12-22 03:25:00 +0900

    2022-12-21,22
    OOP 프로젝트 분할

---
+++ OOP Project 07 File Division/AccountHandler.cpp
@@ -0,0 +1,11 @@
+#include "BankingCommonDecl.h"
+#include "AccountHandler.h"
+#include "Account.h"
+#include "NormalAccount.h"
+#include "HighCreditAccount.h"
+
+void AccountHandler::ShowMenu() const
+{
+	cout << "------Menu------" << endl;
+	cout << "1. °èÁÂ°³¼³" << endl;
+}

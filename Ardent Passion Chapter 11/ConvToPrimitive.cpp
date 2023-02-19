48fe8720e63a13315a4f00c78a7ebfafdd0d68a1
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2023-02-20 05:52:56 +0900

    23-2-20
    추가

--- Ardent Passion Chapter 11/ConvToPrimitive.cpp
+++ Ardent Passion Chapter 11/ConvToPrimitive.cpp
@@ -19,6 +19,7 @@ public:
 	}
 
 	operator int() // 형 변환 연산자의 오버로딩
+		// 반환형이 없음에도 불구하고 반환을 하고 있다는 사실.
 	{
 		return num;
 	}
@@ -31,6 +32,9 @@ int main()
 	Number num1;
 	num1 = 30;
 	Number num2 = num1 + 20;
+	// num1+20이 먼저 진행되어야 하는데 operator+ 함수가 정의되어 있지 않으니
+	// 덧셈이 가능하려면 num1이 int형으로 변환이 되어야 한다.
+	// 이 때 호출되는 것이 형 변환 연산자이다.
 	num2.ShowNumber();
 	return 0;
 }
